fd0287485ea7d3ee4d2db6df5bee1e4d28afe47a
John-Stephenson-STL <john.stephenson.java.jobs@gmail.com>
2018-04-11 15:40:40 -0500

    Add files via upload
    CRUD functionality.  Create Maven to SpringBoot Application.

---
+++ spring-api/src/main/java/com/stephenson/spring/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.stephenson.spring.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi, from STS 3.9.3!";
+    }
+}
